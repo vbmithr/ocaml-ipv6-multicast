42f179debc7f6ba2e4f3c8ae77f3b9a7f5fefb26
Vincent Bernardoff <vb@luminar.eu.org>
2014-09-18 13:37:27 +0200

    .mli cosmetics.

--- lib/sockopt.mli
+++ lib/sockopt.mli
@@ -3,16 +3,17 @@ open Unix
 type ipver = V4 | V6
 
 val if_nametoindex : string -> int
-  (** [if_nametoindex iface] is the index of iface [iface]. *)
+(** [if_nametoindex iface] is the index of iface [iface]. *)
 
-
-val bind6 : ?iface:string -> ?flowinfo:int -> Unix.file_descr -> Ipaddr.V6.t -> int -> unit
+val bind6 : ?iface:string -> ?flowinfo:int -> file_descr ->
+  Ipaddr.V6.t -> int -> unit
 (** [bind6 ~iface ~flow fd v6addr port] binds [fd] to the socket
     address [v6addr:port]. The optional argument [iface] is required
     when binding a link-local multicast address, which is impossible
     to do with {!Unix.bind}. *)
 
-val connect6 : ?iface:string -> ?flowinfo:int -> Unix.file_descr -> Ipaddr.V6.t -> int -> unit
+val connect6 : ?iface:string -> ?flowinfo:int -> file_descr ->
+  Ipaddr.V6.t -> int -> unit
 (** [connect6 ~iface ~flow fd v6addr port] connects [fd] to the socket
     address [v6addr:port]. The optional argument [iface] is required
     when binding a link-local address, which is impossible to do with
@@ -25,7 +26,8 @@ val connect : ?iface:string -> ?flowinfo:int -> file_descr -> sockaddr -> unit
 (** Either uses [Unix.connect] or [connect6] according to [sockaddr]. *)
 
 (* Both IPv4 and IPv6 *)
-val membership           : ?iface:string -> file_descr -> inet_addr -> [< `Join | `Leave ] -> unit
+val membership           : ?iface:string -> file_descr -> inet_addr ->
+  [< `Join | `Leave ] -> unit
 val mcast_outgoing_iface : file_descr -> ipver -> string -> unit
 val mcast_loop           : file_descr -> ipver -> bool -> unit
 val mcast_hops           : file_descr -> ipver -> int -> unit
